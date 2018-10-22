2a29426be1d690b6f90bc45b437900deee46d832
sevenseablue <sevenseablue@gmail.com>
2018-10-22 13:06:15 +0800

    hupu settings

--- src/hupu_spider/settings.py
+++ src/hupu_spider/settings.py
@@ -26,7 +26,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.01
+DOWNLOAD_DELAY = 0.0001
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,14 +64,14 @@ DOWNLOADER_MIDDLEWARES = {
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'hupu_spider.pipelines.HupuSpiderPipeline': 600,
-    'hupu_spider.pipelines.HupuImgDownloadPipeline': 800,
+    'hupu_spider.pipelines.HupuSpiderPipeline': 300,
+    'hupu_spider.pipelines.HupuImgDownloadPipeline': 400,
 }
 
 IMAGES_STORE = '/d/hupu/image/'
 # 90天内抓取的不会再抓
 IMAGES_EXPIRES = 90
-DOWNLOAD_DELAY = 0.01
+DOWNLOAD_DELAY = 0.0001
 # IMAGES_THUMBS = {  # 缩略图的尺寸，设置这个值就会产生缩略图
 #     'small': (200, 200),
 #     'big': (200, 200),
